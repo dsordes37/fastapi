d643b855e8b00f68842dda29980db4339eba2978
D'Sordes <125225427+dsordes37@users.noreply.github.com>
2024-07-13 13:12:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 vendas={
     1:{'item':'lata', 'preco_uni':2.50, 'quantidade':5},
