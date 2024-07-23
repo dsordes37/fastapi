548eb2143540ec5ff73a37b1ffc9535c5c9c0fd0
dsordes <dsordesdinho34@gmail.com>
2024-07-23 19:58:35 -0300

    modularização3

--- src/main.py
+++ src/main.py
@@ -2,45 +2,8 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# from src.bd import consulta
+from src.bd import consulta
 
-
-import mysql.connector as mys
-
-
-conexao = mys.connect(
-    host="monorail.proxy.rlwy.net",
-    port='25969',
-    user="root",
-    password="IDpNJKkbCupRBHxeslRyoYRGfoZTPKmB",
-    database="railway",
-)
-
-
-cursor=conexao.cursor()
-
-
-
-
-
-
-
-def consulta():
-    vendas=[]
-    cursor.execute('select * from Produtos')
-    result=cursor.fetchall()
-
-    for r in result:
-        vendas.append({
-            'id':r[0],
-            'item':r[1],
-            'preco_uni':r[2],
-            'quantidade':r[3]
-        })
-    
-    
-    
-    return vendas
 
 
 
